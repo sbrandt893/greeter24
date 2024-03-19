46a8f97eca13fd90672fc500f14f93964991c953
Stefan Brandt <stefan_brandt@cdemy.de>
2024-03-19 21:27:45 +0100

    first commit

---
+++ bin/greeter24.dart
@@ -0,0 +1,5 @@
+import 'package:greeter24/greeter24.dart' as greeter24;
+
+void main(List<String> arguments) {
+  print('Hello world: ${greeter24.calculate()}!');
+}
---
+++ test/greeter24_test.dart
@@ -0,0 +1,8 @@
+import 'package:greeter24/greeter24.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
